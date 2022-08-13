dad9bfbaabc95cd6d264b6fb39ebadf5f83c857b
Samet Taşcı <samet.tasci@hotmail.com>
2022-08-13 23:43:15 +0300

    Add project files.

---
+++ ConsoleUı/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
